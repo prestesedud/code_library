f4171a7fb3717de45567267d13f39a28942e042c
David Prestes <davidupts@gmail.com>
2026-02-11 20:35:35 -0300

    estudo hook

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
-import { LockKeyhole } from 'lucide-react';
+import { LockKeyhole, MonitorPlay } from 'lucide-react';
 
 const Info = () => {
-  const [soma, setSoma] = useState(0);
+  const [show, setShow] = useState(false);
+
+  // function showLink() {
+  //   setShow(!show);
+  // }
 
   return (
     <div className="mt-6 flex w-full justify-between items-center">
@@ -12,13 +16,22 @@ const Info = () => {
           Em vez de apenas ensinar teoria, focamos em código real para inspirar
           seu próximo projeto. Aprender não precisa ser chato.
         </p>
-        <button
-          className="w-[170px] h-8 bg-[#00A2F7] text-white rounded-md cursor-pointer hover:bg-[#0080c7] transition-colors flex justify-center items-center gap-2"
-          onClick={() => setSoma(soma + 3)}
-        >
-          <LockKeyhole size={16}/>
-          Desbloquear aula
-        </button>
+        <div className="flex items-center gap-4 ">
+          <button
+            className="w-[170px] h-8 bg-[#00A2F7] text-white rounded-md cursor-pointer hover:bg-[#0080c7] transition-colors flex justify-center items-center gap-2"
+            onClick={() => setShow(!show)}
+          >
+            <LockKeyhole size={16} />
+            {show ? "Bloquear Aula" : "Desbloquear Aula"}
+          </button>
+
+          {show && (
+            <p className="flex items-center gap-2 cursor-pointer">
+              <MonitorPlay />
+              Clique aqui para assistir sua aula
+            </p>
+          )}
+        </div>
       </div>
       <div className="text-[#00A2F7] bg-white rounded-md flex flex-col gap-2 p-2">
         <p className="font-bold text-md">Curso rápido</p>
